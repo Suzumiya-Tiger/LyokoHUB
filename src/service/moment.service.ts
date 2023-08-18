df232af4977d3d1b2e3f0a5cb28995670d6e86f2
Suzumiya-Tiger <760229204@qq.com>
2023-08-18 15:29:11 +0800

    feat: query dynamic details - Show a list of comments

--- src/service/moment.service.ts
+++ src/service/moment.service.ts
@@ -33,10 +33,23 @@ FROM moment m
   async queryById(id: number) {
     const statement = `SELECT
 	m.id id,m.content content,m.createAt createTime,m.updateAt updateTime,
-	JSON_OBJECT("id",u.id,'name',u.name,'createTime',u.createAt,'updateTime',u.updateAt) user
-  FROM moment m
-	LEFT JOIN user u ON u.id = m.user_id 
-   WHERE m.id=?;`;
+	JSON_OBJECT(
+    "id",u.id,'name',u.name,'createTime',u.createAt,'updateTime',u.updateAt
+    ) user,
+	(
+	JSON_ARRAYAGG(
+    JSON_OBJECT(
+      'id',c.id,'content',c.content,'commentId',c.comment_id,'createTime',c.createAt,
+      'user',JSON_OBJECT('id',cu.id,'name',cu.name)
+      )
+    )
+	) comments
+FROM moment m
+	LEFT JOIN user u ON u.id = m.user_id
+	LEFT JOIN comment c ON c.moment_id=m.id
+	LEFT JOIN user cu ON cu.id=c.user_id
+  WHERE m.id=?
+GROUP BY m.id;`;
     const [result] = await connection.execute(statement, [id]);
     return result;
   }
